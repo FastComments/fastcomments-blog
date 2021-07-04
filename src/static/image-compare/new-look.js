b74252bb4b0e5c003bddd690bfbc557be7e9bd4f
winrid <winrid@gmail.com>
2021-07-04 12:55:38 -0700

    fixing images not loading

--- src/static/image-compare/new-look.js
+++ src/static/image-compare/new-look.js
@@ -1,22 +1,33 @@
 $(function () {
-    $(".screenshot-comparison").twentytwenty({
-        //  How much of the before image is visible when the page loads
-        default_offset_pct: 0.0,
+    function setup() {
+        $('.screenshot-comparison').twentytwenty({
+            //  How much of the before image is visible when the page loads
+            default_offset_pct: 0.0,
 
-        // or vertical
-        orientation: 'horizontal',
+            // or vertical
+            orientation: 'horizontal',
 
-        // label text
-        before_label: 'Before',
-        after_label: 'After',
+            // label text
+            before_label: 'Before',
+            after_label: 'After',
 
-        // enable/disable overlay
-        no_overlay: false,
+            // enable/disable overlay
+            no_overlay: false,
 
-        // move with handle
-        move_with_handle_only: true,
+            // move with handle
+            move_with_handle_only: true,
 
-        // click to move
-        click_to_move: false
-    });
+            // click to move
+            click_to_move: false
+        });
+    }
+    setup();
+    // Library messes up sometimes. Just retry.
+    const interval = setInterval(function() {
+        if ($('.screenshot-comparison').height() === 0) {
+            setup();
+        } else {
+            clearInterval(interval);
+        }
+    }, 50);
 });
