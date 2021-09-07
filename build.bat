fbc7e303c674d6c08d7ab1b34efef0b4eaeb9bcd
winrid <winrid@gmail.com>
2021-09-06 17:11:00 -0700

    removing unused build steps

--- build.bat
+++ build.bat
@@ -3,4 +3,3 @@ xcopy src\static\css src\static\generated\css\ /E /y
 xcopy src\static\images src\static\generated\images\ /E /y
 xcopy src\static\js src\static\generated\js\ /E /y
 xcopy src\static\image-compare src\static\generated\image-compare\ /E /y
-node node_modules/uglify-js/bin/uglifyjs node_modules/lozad/dist/lozad.js src/static/js/blog.js -o src/static/generated/js/blog.min.js
