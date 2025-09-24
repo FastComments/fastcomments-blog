048b79dcfb7e27f6af2ab53f1da765b9a4b7cb17
winrid <winrid@gmail.com>
2025-09-24 10:34:57 -0700

    optimize build

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-npm install
-node src/app
-mkdir -p src/static/generated/
-cp -rv src/static/css src/static/generated/
-cp -rv src/static/images src/static/generated/
-cp -rv src/static/js src/static/generated/
-cp -rv src/static/image-compare src/static/generated/
-cp src/static/*.html src/static/generated/
+if [ "$PARTIAL_BUILD" != "true" ]; then
+  npm install
+  node src/app
+  mkdir -p src/static/generated/
+  cp -rv src/static/css src/static/generated/
+  cp -rv src/static/images src/static/generated/
+  cp -rv src/static/js src/static/generated/
+  cp -rv src/static/image-compare src/static/generated/
+  cp src/static/*.html src/static/generated/
+fi
